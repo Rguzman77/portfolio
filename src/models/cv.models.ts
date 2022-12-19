2e17214f63940ece273aa53df5b3e3fe54a2fd0d
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2022-12-19 13:19:38 +0100

    solved while importing json files

---
+++ src/models/cv.models.ts
@@ -0,0 +1,9 @@
+//IMPORTAR CONTENIDO BASE DE DATOS
+import {Request, Response} from 'express'
+import {content} from './database.json'
+
+export const personalData = {
+    getAllData: ()=> {
+        return content
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
-console.log("working")
 import express from "express";
 import cors from "cors";
+import router from "./routes/cv.route";
 
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+// START SERVER
+const PORT = 3000
+const server = app.listen(PORT, () => console.log(`server running on port ${PORT}`))
